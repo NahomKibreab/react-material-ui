e8e02057a239b617054ec8e7bb01f49f7fd71411
NahomKibreab <nknahom@gmail.com>
2021-09-25 23:47:56 -0600

    Add NavBar and Card components

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,11 +1,41 @@
-import { Button, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import "./App.css";
+import NavBar from "./NavBar";
+import RecipeReviewCard from "./Card";
 
 function App() {
+  const cars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className="App">
-      <Typography>Hello</Typography>
-      <Button variant="contained">Pdafdfasdfasfadsfafadsf</Button>
+      <Grid container direction="column">
+        <Grid item sx={{ background: "red" }}>
+          <NavBar />
+        </Grid>
+        <Grid
+          item
+          container
+          sx={{ background: "green" }}
+          justifyContent="center"
+        >
+          <Grid item xs={12} md={2} />
+          <Grid
+            item
+            xs={12}
+            md={8}
+            container
+            sx={{ background: "blue" }}
+            spacing={2}
+            justifyContent="space-evenly"
+          >
+            {cars.map((car) => (
+              <Grid item>
+                <RecipeReviewCard />
+              </Grid>
+            ))}
+          </Grid>
+          <Grid item xs={12} md={2} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
